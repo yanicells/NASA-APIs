695048b554e8e26cef645a834de48c9a4c3184ee
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2025-10-02 23:26:49 +0800

    Edit apod

--- index.js
+++ index.js
@@ -34,31 +34,22 @@ app.get("/nasa-library", (req, res) =>{
 
 app.post("/apod-picture", async (req, res) => {
   try {
-    const selectedDate = req.body.selectedDate;
-    const [year, month, day] = selectedDate.split("-");
-    console.log(
-      `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${selectedDate}`
-    );
+    const start_date = req.body.start;
+    const end_date = req.body.end.length === 0 ? start_date : req.body.end;
+    console.log(start_date, end_date);
+    
 
     const result = await axios.get("https://api.nasa.gov/planetary/apod", {
       params: {
         api_key: api_key,
-        date: selectedDate,
+        start_date: start_date, 
+        end_date: end_date,
         hd: false,
       },
     });
 
-    // Specific Date: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2023-12-05
-    // Random Count: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5
-    // Date Range: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-01-01&end_date=2023-01-05
-    // GEMINI: https://gemini.google.com/app/ec7ac7f3cee02dd2
-
     res.render("apod.ejs", {
-      date: result.data.date,
-      text: result.data.explanation,
-      title: result.data.title,
-      image: result.data.hdurl,
-      name: result.data.copyright.trim() || "NASA",
+      apods: result.data,
     });
   } catch (error) {
     console.log(error.response.data);
@@ -95,8 +86,6 @@ app.post("/mars-rover", async (req, res) => {
     const datesList = availableDates
       .map((date) => date.earth_date) // extract earth_date string directly
       .slice(0, 60); // limit to first 60 dates
-
-    console.log(datesList);
 
     res.render("mars-rover.ejs", {
       photos: result.data.photos,
